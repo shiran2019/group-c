ebebfbfd2f51e5e27bb332afdfecccc89bde128f
Shiran Maduwantha <88098649+shiran2019@users.noreply.github.com>
2024-06-13 15:16:13 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import logo from './DecryptogenTech-Logo.png';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="Decryptogen Tech Logo" />
+        <p>Welcome to the Devops Workshop 2024</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
